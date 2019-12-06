ec09b4e9ae1b4e2f073c8dfdd2994c3f1747e183
Andrew Willette <willette.andrew1846@gmail.com>
2019-12-05 23:07:07 -0500

    Configuring metalsharp. Not yet operational

---
+++ andrewwilletteGenerator/andrewwilletteGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Metalsharp;
+
+namespace andrewwilletteGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			new MetalsharpProject("Site");
+		}
+	}
+}
---
+++ andrewwilletteGenerator/andrewwilletteGenerator/Site/index.md
@@ -0,0 +1,1 @@
+Andrew Willette Home Page of Website
