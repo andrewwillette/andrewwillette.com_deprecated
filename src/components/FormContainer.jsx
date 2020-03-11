994e2945c8446dc80ed4a656b4f9631e0e413111
andrewwillette <willette.andrew1846@gmail.com>
2020-03-10 20:06:07 -0500

    adding lulus back in town

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import ReactAudioPlayer from 'react-audio-player';
-import track1 from "../../assets/audio/bach_louree_c_major_partitita.wav";
+import bach_lourree from "../../assets/audio/bach_louree_c_major_partitita.wav";
+import lulus_back_in_town from "../../assets/audio/lulus_back_in_town.wav";
 
 class FormContainer extends Component {
   constructor() {
@@ -14,16 +15,24 @@ class FormContainer extends Component {
   render() {
     const { } = this.state;
     return (
-
+<div>
         <ReactAudioPlayer
         className="center"
-        id="first-track"
-  src={track1}
+        id="bach_lourree"
+  src={bach_lourree}
   autoPlay={false}
   controls
   volume={.5}
 />
-
+<ReactAudioPlayer
+        className="center"
+        id="lulus_back_in_town"
+  src={lulus_back_in_town}
+  autoPlay={false}
+  controls
+  volume={.5}
+/>
+</div>
     );
   }
 }
