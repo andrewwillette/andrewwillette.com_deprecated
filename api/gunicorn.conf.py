6f3589a115b30373e7635ecb36865c4a0cf384c6
andrewwillette <willette.andrew1846@gmail.com>
2020-08-28 17:39:23 -0500

    logging working correctly now with ignores/generation

--- api/gunicorn.conf.py
+++ api/gunicorn.conf.py
@@ -1,4 +1,15 @@
+import os.path
+
 daemon=True
 capture_output=True
-accesslog = './logs/access_log.txt'
-errorlog = './logs/error_log.txt'
+
+accesslog_location = './logs/access_log.txt'
+errorlog_location = './logs/error_log.txt'
+
+for location in [accesslog_location, errorlog_location]:
+    if not os.path.exists(location):
+        f = open(location,'w+')
+        f.close()
+
+accesslog = accesslog_location
+errorlog = errorlog_location
