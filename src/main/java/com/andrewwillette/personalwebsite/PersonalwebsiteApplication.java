b69cea301c0d20a508c210b03356d311158687dc
andrewwillette <willette.andrew1846@gmail.com>
2020-11-11 19:38:48 -0600

    initial spring boot implementation. react logic all moved into ./frontend folder, starting to get into business. we out here

---
+++ src/main/java/com/andrewwillette/personalwebsite/PersonalwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.andrewwillette.personalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/andrewwillette/personalwebsite/controllers/HomeController.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+class HomeController {
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+}
