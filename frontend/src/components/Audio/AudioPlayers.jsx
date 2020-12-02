c7874e9de8c538f9cf973649d0db01cd703ddbfb
andrewwillette <willette.andrew1846@gmail.com>
2020-12-01 20:16:39 -0600

    spring boot now correctly hosting static content, god have mercy on my soul this was not simple. So much documentation, so much bad documentation

--- frontend/src/components/Audio/AudioPlayers.jsx
+++ frontend/src/components/Audio/AudioPlayers.jsx
@@ -16,41 +16,48 @@ export default class AudioPlayers extends Component {
         <div className="row">
           <div className="col-md">
             <div>
-            <AudioPlayer 
-              soundcloudSource="https://soundcloud.com/user-434601011/bach-louree-c-major-partitita?show_teaser=false"
-            ></AudioPlayer>
+            <AudioPlayer
+                soundcloudSource="https://soundcloud.com/user-434601011/bach-louree-c-major-partitita?show_teaser=false"
+            />
             </div>
           </div>
           <div className="col-md">
           <AudioPlayer
-            soundcloudSource="https://soundcloud.com/user-434601011/somewhereovertherainbow?show_teaser=false"
-          ></AudioPlayer>
+              soundcloudSource="https://soundcloud.com/user-434601011/somewhereovertherainbow?show_teaser=false"
+          />
           </div>
         </div>
         
         <div className="row">
           <div className="col-md">
             <AudioPlayer
-              soundcloudSource="https://soundcloud.com/user-434601011/there-will-never-be-another-you-take3?show_teaser=false"
-            ></AudioPlayer>
+                soundcloudSource="https://soundcloud.com/user-434601011/there-will-never-be-another-you-take3?show_teaser=false"
+            />
           </div>
           <div className="col-md">
             <AudioPlayer
                 soundcloudSource="https://soundcloud.com/user-434601011/capri?show_teaser=false"
-            ></AudioPlayer>
+            />
           </div>
         </div>
         <div className="row">
             <div className="col-md">
                 <AudioPlayer
-                soundcloudSource="https://soundcloud.com/user-434601011/these-foolish-things?show_teaser=false"
-                ></AudioPlayer>
+                    soundcloudSource="https://soundcloud.com/user-434601011/these-foolish-things?show_teaser=false"
+                />
             </div>
             <div className="col-md">
-                {/* <AudioPlayer
-                soundcloudSource="https://soundcloud.com/user-434601011/these-foolish-things?show_teaser=false"
-                ></AudioPlayer> */}
+                <AudioPlayer
+                    soundcloudSource="https://soundcloud.com/user-434601011/dont-do-me-like-that?show_teaser=false"
+                />
             </div>
+          </div>
+          <div className="row">
+              <div className="col-md">
+                  <AudioPlayer
+                      soundcloudSource="https://soundcloud.com/user-434601011/billy-on-the-lowground?show_teaser=false"
+                  />
+              </div>
           </div>
       </React.Fragment>
     );
--- src/main/java/com/andrewwillette/personalwebsite/controllers/HomeController.java
+++ src/main/java/com/andrewwillette/personalwebsite/controllers/HomeController.java
@@ -1,13 +1,45 @@
+package com.andrewwillette.personalwebsite.controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.*;
 
-@RestController
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Controller
 class HomeController {
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @RequestMapping(value="/")
     public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value="/music")
+    public String music() {
        return "index";
+    }
+
+    @RequestMapping(value="/resume")
+    public String resume() {
+        return "index";
+    }
+
+    //TODO: Burn this
+    @RequestMapping(value="/test")
+    public Object test() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        HashMap jsonObject = new HashMap<>();
+        jsonObject.put("samplekey", "sampleValue");
+        Object toReturn = objectMapper.writeValueAsString(jsonObject);
+        return toReturn;
     }
 }
